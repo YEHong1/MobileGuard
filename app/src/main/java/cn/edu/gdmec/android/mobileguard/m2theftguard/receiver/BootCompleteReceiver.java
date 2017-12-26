19659d25c67913eed42059fc473fd2f1e8e9c4fb
1814213263 <1814213263@qq.com>
2017-12-26 10:41:11 +0800

    Last1

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/receiver/BootCompleteReceiver.java
@@ -0,0 +1,17 @@
+package cn.edu.gdmec.android.mobileguard.m2theftguard.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import cn.edu.gdmec.android.mobileguard.App;
+import cn.edu.gdmec.android.mobileguard.m9advancedtools.service.AppLockService;
+
+public class BootCompleteReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        ((App)(context.getApplicationContext())).correctSIM();
+        context.startService(new Intent(context, AppLockService.class));
+    }
+}
